c6da5558f7bc9cd29185da949a091ace7d39fec5
Kyle Amonson <kyle.amonson@IntelliTect.com>
2019-03-22 16:02:49 -0700

    intial commit

---
+++ MorseCodeInterpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MorseCodeInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
