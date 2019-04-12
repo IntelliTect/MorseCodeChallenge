b548ad6c9ccda03e8afdbdf2f2c542ec8dddf5ba
Sean Scura <sean.scura@IntelliTect.com>
2019-04-12 15:21:30 -0700

    V.1 of C# morse code interpreter by Amonson, Curn and Scura.

---
+++ MorseCodeInterpreter/Dash.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorseCodeInterpreter
+{
+    public class Dash : Unit
+    {
+    }
+}
---
+++ MorseCodeInterpreter/Dot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorseCodeInterpreter
+{
+    public class Dot : Unit
+    {
+
+    }
+}
